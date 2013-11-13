43968e80d959e19c3440eb9871a2059abc9dd96e
Jamie Hillman <jamie.hillman@ukfuels.co.uk>
2013-11-13 09:47:20 +0000

    pep8ified

--- djangostack/__init__.py
+++ djangostack/__init__.py
@@ -2,57 +2,63 @@ from taskset import TaskSet, task_method
 from fabric.api import *
 from fabric.contrib.files import exists
 from cuisine import *
-from cuisine_postgresql import postgresql_role_ensure, postgresql_database_ensure
+from cuisine_postgresql import postgresql_role_ensure, \
+    postgresql_database_ensure
 import time
+
 
 class DjangoStack(TaskSet):
 
-    DEFAULT_PYTHON_DEPENDENCIES=['psycopg2','south']
-    DEFAULT_ADDITIONAL_PACKAGES=['vim',]
-    DEFAULT_DJANGO_ADMIN_USER='admin'
-    DEFAULT_DJANGO_ADMIN_PASSWORD='notagoodpassword'
-    DEFAULT_DJANGO_ADMIN_EMAIL='admin@example.com'
+    DEFAULT_PYTHON_DEPENDENCIES = ['psycopg2', 'south']
+    DEFAULT_ADDITIONAL_PACKAGES = ['vim', ]
+    DEFAULT_DJANGO_ADMIN_USER = 'admin'
+    DEFAULT_DJANGO_ADMIN_PASSWORD = 'notagoodpassword'
+    DEFAULT_DJANGO_ADMIN_EMAIL = 'admin@example.com'
 
-    def __init__(self,projectName,apacheConfigName='apache_site',scmType='mercurial',databaseName=None,databaseUser=None,databasePassword=None,djangoProjectPath=None,
-                    djangoAdminUser=DEFAULT_DJANGO_ADMIN_USER, djangoAdminEmail=DEFAULT_DJANGO_ADMIN_EMAIL,
-                    djangoAdminPassword=DEFAULT_DJANGO_ADMIN_PASSWORD,databaseDumpType='SQL',djangoVersionNumber=""):
-        self.PROJECT_NAME=projectName
-        self.PYTHON_DEPENDENCIES=self.DEFAULT_PYTHON_DEPENDENCIES
-        if djangoVersionNumber!="":
-            self.PYTHON_DEPENDENCIES.append("django==%s"%djangoVersionNumber)
+    def __init__(self, projectName, apacheConfigName='apache_site',
+                 scmType='mercurial', databaseName=None, databaseUser=None,
+                 databasePassword=None, djangoProjectPath=None,
+                 djangoAdminUser=DEFAULT_DJANGO_ADMIN_USER,
+                 djangoAdminEmail=DEFAULT_DJANGO_ADMIN_EMAIL,
+                 djangoAdminPassword=DEFAULT_DJANGO_ADMIN_PASSWORD,
+                 databaseDumpType='SQL',
+                 djangoVersionNumber=""):
+        self.PROJECT_NAME = projectName
+        self.PYTHON_DEPENDENCIES = self.DEFAULT_PYTHON_DEPENDENCIES
+        if djangoVersionNumber != "":
+            self.PYTHON_DEPENDENCIES.append("django==%s" % djangoVersionNumber)
         else:
             self.PYTHON_DEPENDENCIES.append("django")
-        self.SCM_TYPE=scmType
-        self.DATABASE_NAME=databaseName
-        self.DATABASE_USER=databaseUser
-        self.DATABASE_PASSWORD=databasePassword
-        self.DJANGO_PROJECT_PATH=djangoProjectPath
-        self.DJANGO_ADMIN_USER=djangoAdminUser
-        self.DJANGO_ADMIN_PASSWORD=djangoAdminPassword
-        self.DJANGO_ADMIN_EMAIL=djangoAdminEmail
-        self.APACHE_CONFIG_NAME=apacheConfigName
-        self.DATABASE_DUMP_TYPE=databaseDumpType
-        self.repositories=[]
-        self.packages=[]
+        self.SCM_TYPE = scmType
+        self.DATABASE_NAME = databaseName
+        self.DATABASE_USER = databaseUser
+        self.DATABASE_PASSWORD = databasePassword
+        self.DJANGO_PROJECT_PATH = djangoProjectPath
+        self.DJANGO_ADMIN_USER = djangoAdminUser
+        self.DJANGO_ADMIN_PASSWORD = djangoAdminPassword
+        self.DJANGO_ADMIN_EMAIL = djangoAdminEmail
+        self.APACHE_CONFIG_NAME = apacheConfigName
+        self.DATABASE_DUMP_TYPE = databaseDumpType
+        self.repositories = []
+        self.packages = []
         self.packages.extend(self.DEFAULT_ADDITIONAL_PACKAGES)
 
-        self.postBuildHooks=[]
+        self.postBuildHooks = []
 
         # check for deploy key file
 
         # check for apache config file
 
-
-    def addAdditionalPythonDep(self,dependency):
+    def addAdditionalPythonDep(self, dependency):
         self.PYTHON_DEPENDENCIES.append(dependency)
 
-    def addAdditionalPackage(self,packageName):
+    def addAdditionalPackage(self, packageName):
         self.packages.append(packageName)
 
-    def addCheckout(self,sourceRepository,destination):
-        self.repositories.append([sourceRepository,destination])
+    def addCheckout(self, sourceRepository, destination):
+        self.repositories.append([sourceRepository, destination])
 
-    def addPostBuildHook(self,func):
+    def addPostBuildHook(self, func):
         self.postBuildHooks.append(func)
 
     @task_method
@@ -72,7 +78,7 @@ class DjangoStack(TaskSet):
         self.createDatabaseUser()
         self.createDatabase()
         self.createApacheSite()
-        
+
         self.setupBitbucketKey()
         self.checkoutCode()
 
@@ -89,13 +95,13 @@ class DjangoStack(TaskSet):
 
     @task_method
     def checkoutCode(self):
-        if self.SCM_TYPE.lower()=='mercurial':
-            scm_command="hg clone"
-        elif self.SCM_TYPE.lower()=='git':
-            scm_command="git clone"
+        if self.SCM_TYPE.lower() == 'mercurial':
+            scm_command = "hg clone"
+        elif self.SCM_TYPE.lower() == 'git':
+            scm_command = "git clone"
         for sourceRepository, destination in self.repositories:
             with mode_sudo():
-                run("%s %s %s"%(scm_command,sourceRepository,destination))
+                run("%s %s %s" % (scm_command, sourceRepository, destination))
 
     @task_method
     def setupPython(self):
@@ -106,58 +112,62 @@ class DjangoStack(TaskSet):
 
         with mode_sudo():
             for dependency in self.PYTHON_DEPENDENCIES:
-                run("pip install %s"%dependency)
+                run("pip install %s" % dependency)
 
     @task_method
     def setupApache(self):
-        hadApache=package_ensure("apache2")
+        hadApache = package_ensure("apache2")
 
         package_ensure("libapache2-mod-wsgi")
         with mode_sudo():
             run("a2enmod rewrite")
 
-        if not hadApache and hasattr(env,"vagrantMode"):
+        if not hadApache and hasattr(env, "vagrantMode"):
             local("vagrant reload")
             time.sleep(15)
-    
 
     @task_method
     def useVagrant(self):
         env.user = 'vagrant'
         env.hosts = ['127.0.0.1:2222']
-        env.vagrantMode=True
-  
+        env.vagrantMode = True
+
         # retrieve the IdentityFile:
         result = local('vagrant ssh-config | grep IdentityFile', capture=True)
-        env.key_filename = result.split()[1][1:-1] # parse IdentityFile
-
+        env.key_filename = result.split()[1][1:-1]  # parse IdentityFile
 
     @task_method
     def setupPostgres(self):
         package_ensure("postgresql")
         package_ensure("postgresql-client")
         package_ensure("libpq-dev")
-        
 
     @task_method
     def setupScm(self):
-        if self.SCM_TYPE=='mercurial':
+        if self.SCM_TYPE == 'mercurial':
             package_ensure("mercurial")
-        elif self.SCM_TYPE.lower()=='git':
+        elif self.SCM_TYPE.lower() == 'git':
             package_ensure("git")
-        
 
     @task_method
     def createDatabaseUser(self):
-        postgresql_role_ensure(self.DATABASE_USER,self.DATABASE_PASSWORD,createdb=True)
+        postgresql_role_ensure(self.DATABASE_USER,
+                               self.DATABASE_PASSWORD,
+                               createdb=True)
 
     @task_method
     def createDatabase(self):
-        postgresql_database_ensure(self.DATABASE_NAME,owner=self.DATABASE_USER,encoding='utf8',template='template0',locale='en_US.UTF-8')
+        postgresql_database_ensure(self.DATABASE_NAME,
+                                   owner=self.DATABASE_USER,
+                                   encoding='utf8',
+                                   template='template0',
+                                   locale='en_US.UTF-8')
 
     @task_method
     def createApacheSite(self):
-        put("%s"%self.APACHE_CONFIG_NAME,"/etc/apache2/sites-enabled/%s"%self.PROJECT_NAME,use_sudo=True)
+        put("%s" % self.APACHE_CONFIG_NAME,
+            "/etc/apache2/sites-enabled/%s" % self.PROJECT_NAME,
+            use_sudo=True)
 
         if exists("/etc/apache2/sites-enabled/000-default"):
             sudo("rm /etc/apache2/sites-enabled/000-default")
@@ -166,28 +176,39 @@ class DjangoStack(TaskSet):
     def setupBitbucketKey(self):
         with mode_sudo():
             dir_ensure("/root/.ssh/")
-        put("deploykey","~/id_rsa")
-        put("deploykey.pub","~/id_rsa.pub")
+        put("deploykey", "~/id_rsa")
+        put("deploykey.pub", "~/id_rsa.pub")
         sudo("mv ~/id_rsa /root/.ssh/")
         sudo("mv ~/id_rsa.pub /root/.ssh/")
-        bitbuckethost="bitbucket.org ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAubiN81eDcafrgMeLzaFPsw2kNvEcqTKl/VqLat/MaB33pZy0y3rJZtnqwR2qOOvbwKZYKiEO1O6VqNEBxKvJJelCq0dTXWT5pbO2gDXC6h6QDXCaHo6pOHGPUy+YBaGQRGuSusMEASYiWunYN0vCAI8QaXnWMXNMdFP3jHAJH0eDsoiGnLPBlBp4TNm6rYI74nMzgz3B9IikW4WVK+dc8KZJZWYjAuORU3jc1c/NPskD2ASinf8v3xnfXeukU0sJ5N6m5E8VLjObPEO+mN2t/FZTMZLiFqPWc/ALSqnMnnhwrNi2rbfg/rd/IpL8Le3pSBne8+seeFVBoGqzHM9yXw=="
-        sudo("echo '%s' >> /root/.ssh/known_hosts"%bitbuckethost)
+        bitbuckethost = "bitbucket.org ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAu" \
+            "biN81eDcafrgMeLzaFPsw2kNvEcqTKl/VqLat/MaB33pZy0y3rJZtnqwR2qOOvb" \
+            "wKZYKiEO1O6VqNEBxKvJJelCq0dTXWT5pbO2gDXC6h6QDXCaHo6pOHGPUy+YBaG" \
+            "QRGuSusMEASYiWunYN0vCAI8QaXnWMXNMdFP3jHAJH0eDsoiGnLPBlBp4TNm6rY" \
+            "I74nMzgz3B9IikW4WVK+dc8KZJZWYjAuORU3jc1c/NPskD2ASinf8v3xnfXeukU" \
+            "0sJ5N6m5E8VLjObPEO+mN2t/FZTMZLiFqPWc/ALSqnMnnhwrNi2rbfg/rd/IpL8" \
+            "Le3pSBne8+seeFVBoGqzHM9yXw=="
+        sudo("echo '%s' >> /root/.ssh/known_hosts" % bitbuckethost)
 
     @task_method
     def restoreDatabaseDump(self):
         if not exists("/var/lib/postgresql/dbdump.txt"):
-            put("dbdump.txt","/var/lib/postgresql/",use_sudo=True)
+            put("dbdump.txt", "/var/lib/postgresql/", use_sudo=True)
         sudo("chown postgres /var/lib/postgresql/dbdump.txt")
 
-        if self.DATABASE_DUMP_TYPE=="SQL":
-            sudo("cd /var/lib/postgresql; psql %s < dbdump.txt"%self.DATABASE_NAME,user="postgres")
+        if self.DATABASE_DUMP_TYPE == "SQL":
+            sudo("cd /var/lib/postgresql;"
+                 "psql %s < dbdump.txt" % self.DATABASE_NAME, user="postgres")
         else:
-            sudo("cd /var/lib/postgresql; pg_restore -d %s dbdump.txt"%self.DATABASE_NAME,user="postgres")
+            sudo("cd /var/lib/postgresql;"
+                 "pg_restore -d %s dbdump.txt" % self.DATABASE_NAME,
+                 user="postgres")
 
     @task_method
     def syncDb(self):
         with mode_sudo():
-            run("cd %s;python manage.py syncdb --noinput; python manage.py migrate --noinput;"%(self.DJANGO_PROJECT_PATH))
+            run("cd %s;python manage.py syncdb --noinput;"
+                "python manage.py migrate --noinput;" %
+                (self.DJANGO_PROJECT_PATH))
 
     @task_method
     def restartServices(self):
@@ -197,6 +218,10 @@ class DjangoStack(TaskSet):
     @task_method
     def createDjangoAdminUser(self):
         # TODO: make this idempotent:
-        sudo("cd %s;echo \"from django.contrib.auth.models import User; User.objects.create_superuser('%s', '%s', '%s')\" | ./manage.py shell"%(self.DJANGO_PROJECT_PATH,self.DJANGO_ADMIN_USER,self.DJANGO_ADMIN_EMAIL,self.DJANGO_ADMIN_PASSWORD))
-
-
+        sudo("cd %s;echo \"from django.contrib.auth.models import User;"
+             "User.objects.create_superuser('%s', '%s', '%s')\""
+             "| ./manage.py shell" %
+             (self.DJANGO_PROJECT_PATH,
+              self.DJANGO_ADMIN_USER,
+              self.DJANGO_ADMIN_EMAIL,
+              self.DJANGO_ADMIN_PASSWORD))
